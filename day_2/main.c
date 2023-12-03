78555f34002635087516189a57a5de1963599494
Mansur <mensurowary@gmail.com>
2023-12-03 00:35:41 -0700

    day-2-part-2

--- day_2/main.c
+++ day_2/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define GAMES 100
-#define COLORS 3
+int max(int a, int b) {
+    if (a > b) {
+        return a;
+    }
+    return b;
+}
 
 int main() {
     FILE* file = fopen("./input", "r");
@@ -11,7 +15,8 @@ int main() {
 
     int line = 1;
 
-    int sum = 0;
+    int sum_of_possible_lines = 0;
+    long sum_of_cubes = 0;
 
     while (fgets(buf, 1024, file)) {
         int i = 0;
@@ -24,14 +29,20 @@ int main() {
         int r = 0;
         int g = 0;
         int b = 0;
+
+        int rm = 0;
+        int gm = 0;
+        int bm = 0;
+
         int possible = 1;
         while (buf[i] != '\n' && buf[i] != 0) {
             if (buf[i] == ';') { // each hand is delimited by ;
-                if (!(r <= 12 && g <= 13 && b <= 14)) {
-                    possible = 0;
-                    break;
-                }
-
+                possible = possible && (r <= 12 && g <= 13 && b <= 14);
+                
+                rm = max(rm, r);
+                gm = max(gm, g);
+                bm = max(bm, b);
+                
                 r = 0;
                 g = 0;
                 b = 0;
@@ -39,32 +50,38 @@ int main() {
             }
             if (buf[i] >= '0' && buf[i] <= '9') {
                 int s = i; // start
-                int num = 0;
                 while (buf[i] != ' ') {
                     i++;
                 }
 
-                num += atoi(buf+s);
+                int num = atoi(buf+s);
 
                 int ch = buf[++i];
                 if (ch == 'r') {
-                    r += num;
+                    r = num;
                 } else if (ch == 'g') {
-                    g += num;
+                    g = num;
                 } else {
-                    b += num;
+                    b = num;
                 }
             }
             i++;
         }
+        rm = max(rm, r);
+        gm = max(gm, g);
+        bm = max(bm, b);
+
+        sum_of_cubes += (rm * gm * bm);
+
         if (possible && (r <= 12 && g <= 13 && b <= 14)) {
-            sum += line;
+            sum_of_possible_lines += line;
         }
 
         line++;
     }
 
-    printf("Part 1 answer: %d\n", sum);
+    printf("Part 1 answer: %d\n", sum_of_possible_lines);
+    printf("Part 2 answer: %ld\n", sum_of_cubes);
 
     fclose(file);
     return 0;
