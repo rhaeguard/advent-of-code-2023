9793a8f272d78ba1ad585c6ee88126066549c82b
Mansur <mensurowary@gmail.com>
2023-12-09 00:10:16 -0700

    day-9-updated

--- day_9/main.c
+++ day_9/main.c
@@ -3,46 +3,51 @@
 
 #define ELEMENT_COUNT 21
 
-int check_if_all_zeros(long* arr[ELEMENT_COUNT], int len) {
+int check_if_all_zeros(long* arr, int len) {
     for (int i = 0; i < len; i++) {
-        if (arr[i] != 0) {
-            return 0;
-        }
+        if (arr[i] != 0) {return 0;}
     }
     return 1;
 }
 
-void reduce(long* input[ELEMENT_COUNT], long* next_sum, long* prev_sum) {
-    long steps[ELEMENT_COUNT+1][ELEMENT_COUNT+1] = {{0}};
+void reduce(
+    long* input, 
+    long* next_sum, 
+    long* prev_sum
+) {
+    long steps[ELEMENT_COUNT][ELEMENT_COUNT] = {{0}};
 
     for (int i=0; i < ELEMENT_COUNT; i++) {
         steps[0][i] = input[i];
     }
 
-    int j = 0;
-    int len = ELEMENT_COUNT;
-    long t = input[ELEMENT_COUNT-1];
-    long z = input[0];
-    long sign = -1;
-    while (!check_if_all_zeros(steps[j], len)) {
-        long* elements = steps[j];
-        if (j == ELEMENT_COUNT - 1) {
-            break;
-        }
+    int j = 0; // indicates the depth
+    int len = ELEMENT_COUNT; // the length of the current array
 
+    long next = input[ELEMENT_COUNT-1];
+    long prev = input[0];
+    long sign = -1;
+    
+    while (
+        !check_if_all_zeros(steps[j], len) && 
+        j != ELEMENT_COUNT - 1 // boundary check
+    ) {
+        long* elements = steps[j];
         for (int k=0; k < len-1; k++) {
-            long a = elements[k+1] - elements[k];
-            steps[j+1][k] = a;
+            steps[j+1][k] = elements[k+1] - elements[k];
         }
-        t += steps[j+1][len-2];
-        z = z + sign*steps[j+1][0];
-        sign = sign * -1;
+        next += steps[j+1][len-2];
+
+        // the pattern is: N0 - N1 + N2 - N3 + N4 - N5....
+        prev = prev + sign*steps[j+1][0];
+        sign = sign * -1; 
+
         j++;
         len--;
     }
 
-    *prev_sum = *prev_sum + z;
-    *next_sum = *next_sum + t;
+    *prev_sum = *prev_sum + prev;
+    *next_sum = *next_sum + next;
 }
 
 int main() {
@@ -60,21 +65,19 @@ int main() {
         while (buf[i] != '\n') {
             input[n] = atol(buf+i);
             
-            while (buf[i] != ' ') {i++;}
-            while (buf[i] == ' ') {i++;}
+            while (buf[i] != ' ') {i++;} // skip the non-whitespace chars
+            while (buf[i] == ' ') {i++;} // skip whitespace chars
 
             n++;
             if (n >= ELEMENT_COUNT) {
                 break;
             }
         }
-
         reduce(input, &next_sum, &prev_sum);
-
     }
 
-    printf("Part 1 answer: %ld\n", next_sum);
-    printf("Part 2 answer: %ld\n", prev_sum);
+    printf("Part 1 answer: %ld\n", next_sum); // 1681758908
+    printf("Part 2 answer: %ld\n", prev_sum); // 803
 
     fclose(file);
     return 0;
