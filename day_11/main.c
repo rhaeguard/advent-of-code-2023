5d6e044e4a79ba16c0597d4c7b4e25c28d3a0d3e
Mansur <mensurowary@gmail.com>
2023-12-11 03:56:46 -0700

    day-11-updated

--- day_11/main.c
+++ day_11/main.c
@@ -7,14 +7,11 @@
 typedef struct Galaxy {
     int row;
     int col;
-    long depth;
+    long p1_depth;
+    long p2_depth;
 } Galaxy;
 
-char decide_directions2(Galaxy* from, Galaxy* to) {
-    // int xdiff = to->col - from->col;
-    // int ydiff = to->row - from->row;
-    // int dist = xdiff*xdiff + ydiff*ydiff;
-
+char decide_directions(Galaxy* from, Galaxy* to) {
     int rr = from->row;
     int cc = from->col;
 
@@ -60,23 +57,23 @@ char decide_directions2(Galaxy* from, Galaxy* to) {
 }
 
 
-int bfs2(Galaxy* from, Galaxy* to, int expanded_rows[SIZE], int expanded_cols[SIZE]) {
-    long depth = 0;
+Galaxy* bfs(Galaxy* from, Galaxy* to, int expanded_rows[SIZE], int expanded_cols[SIZE]) {
     Galaxy* current = malloc(sizeof(Galaxy));
     current->row = from->row;
     current->col = from->col;
-    current->depth = 0;
+    current->p1_depth = 0;
+    current->p2_depth = 0;
     int seen_rows[SIZE] = {0}; 
     int seen_cols[SIZE] = {0};
     while(1) {
         if (current->row == to->row && current->col == to->col) {
-            depth = current->depth;
-            break;
+            return current;
         }
 
-        long new_depth = current->depth + 1;
+        long new_p1_depth = current->p1_depth + 1;
+        long new_p2_depth = current->p2_depth + 1;
 
-        char d = decide_directions2(current, to);
+        char d = decide_directions(current, to);
         int r = current->row;
         int c = current->col;
         
@@ -85,14 +82,16 @@ int bfs2(Galaxy* from, Galaxy* to, int expanded_rows[SIZE], int expanded_cols[SI
             
             if (!seen_cols[c] && expanded_cols[c]) {
                 seen_cols[c] = 1;
-                new_depth += 999999; 
+                new_p1_depth += 1;
+                new_p2_depth += 999999;
             }    
         } else if (d == 'r') {
             c++;
 
             if (!seen_cols[c] && expanded_cols[c]) {
                 seen_cols[c] = 1;
-                new_depth += 999999; 
+                new_p1_depth += 1;
+                new_p2_depth += 999999;
             }
         } else {
             // d
@@ -100,19 +99,19 @@ int bfs2(Galaxy* from, Galaxy* to, int expanded_rows[SIZE], int expanded_cols[SI
 
             if (!seen_rows[r] && expanded_rows[r]) {
                 seen_rows[r] = 1;
-                new_depth += 999999; 
+                new_p1_depth += 1;
+                new_p2_depth += 999999;
             }
         }
 
         current = realloc(current, sizeof(Galaxy));
         current->row = r;
         current->col = c;
-        current->depth = new_depth;
+        current->p1_depth = new_p1_depth;
+        current->p2_depth = new_p2_depth;
     }
 
-    // printf("\n(%d %d) + (%d %d): %d\n", from->row, from->col, to->row, to->col, depth);
-
-    return depth;
+    return NULL;
 }
 
 int main() {
@@ -120,6 +119,7 @@ int main() {
     Galaxy galaxies[COUNT_GALAXY];
     int expanded_rows[SIZE] = {0};
     int expanded_cols[SIZE] = {0};
+
     FILE* file = fopen("./input", "r");
     
     char buf[510];
@@ -157,19 +157,22 @@ int main() {
             }
         }
     }
-    int c = 0;
-    long sum = 0;
+
+    long sum_p1 = 0;
+    long sum_p2 = 0;
     for (int i = 0; i < COUNT_GALAXY; i++) {
         Galaxy from = galaxies[i];
 
         for (int j=i+1; j < COUNT_GALAXY; j++) {
             Galaxy to = galaxies[j];
-            sum += bfs2(&from, &to, expanded_rows, expanded_cols);
+            Galaxy* resp = bfs(&from, &to, expanded_rows, expanded_cols);
+            sum_p1 += resp->p1_depth;
+            sum_p2 += resp->p2_depth;
         }
-        // break;
     }
 
-    printf("Part 1 answer: %ld\n", sum);
+    printf("Part 1 answer: %ld\n", sum_p1);
+    printf("Part 2 answer: %ld\n", sum_p2);
 
     fclose(file);
     return 0;
